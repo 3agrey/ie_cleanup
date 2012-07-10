77c29860f22e4d8b9078d0309f5d74772c50bcce
Sergey Korenkov <3ug3ag@gmail.com>
2012-07-10 17:37:17 +0300

    first commit

---
+++ ie_cleaner/Cleaner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace IE_Cleaner
+{
+    class Cleaner
+    {
+        public static void ClearFolder(DirectoryInfo Path)
+        {
+            foreach (FileInfo File in Path.GetFiles())
+            {
+                try
+                {
+                    File.Delete();
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            foreach (DirectoryInfo Folder in Path.GetDirectories())
+            {
+                ClearFolder(Folder);
+            }
+        }
+    }
+}
---
+++ ie_cleaner/IX_Cleanup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace IE_Cleaner
+{
+    class IX_Cleanup
+    {
+        public static void Main()
+        {
+            Cleaner.ClearFolder(new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache)));
+        }
+
+    }
+}
